44b6543ff144524151fc5906c8d729962eeac485
Yasser Jiménez Martínez <yasserjimenezmartinez@hotmail.com>
2022-03-20 16:21:17 -0600

    Se ha diseñado la vista de permisos para gps

---
+++ lib/screens/gps_access_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+
+class GpsAccessScreen extends StatelessWidget {
+
+  const GpsAccessScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: _AccessButton(),
+     ),
+   );
+  }
+}
+
+class _AccessButton extends StatelessWidget {
+  const _AccessButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Es necesario el acceso a GPS'),
+        MaterialButton(
+          child: const Text('Solicitar Acceso', style: TextStyle(color: Colors.white)),
+          color: Colors.black,
+          shape: const StadiumBorder(),
+          elevation: 0,
+          splashColor: Colors.transparent,
+          onPressed: () {
+            // TODO: Por hacer
+          }
+        )
+      ],
+    );
+  }
+}
+
+class _EnableGpsMessage extends StatelessWidget {
+  const _EnableGpsMessage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Debe de habiltiar el GPS',
+      style: TextStyle( fontSize: 25, fontWeight: FontWeight.w300),
+      );
+  }
+}
